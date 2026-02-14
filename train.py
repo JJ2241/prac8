e4588571ded8ee69e5b7644407399295e99cf0b3
unknown <juni9666@gmail.com>
2026-02-14 12:46:59 +0530

    Added CI pipeline

--- train.py
+++ train.py
@@ -3,6 +3,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 
+print("Training started...")
+
 def train_model():
     data = load_iris()
     X = data.data
